a03f12e58a5c3b01b34c39ab4eb35beb3ee78a06
JayGrey <k_lite@ukr.net>
2021-03-12 09:12:40 +0200

    ex 1.4.24

---
+++ src/org/example/ex/_1/_4/_24/ThrowingEggs.java
@@ -0,0 +1,9 @@
+package org.example.ex._1._4._24;
+
+import org.example.ex._1._4._10.Search;
+
+public class ThrowingEggs {
+    public static int find(int[] array) {
+        return Search.smallestRank(array, 1);
+    }
+}
