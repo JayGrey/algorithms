336903b36e8d1e75e603fd82bef162b47a19eabb
JayGrey <k_lite@ukr.net>
2021-03-25 14:51:10 +0200

    ex 1.5.17

---
+++ src/org/example/ex/_1/_5/_17/ErdosRenyi.java
@@ -0,0 +1,35 @@
+package org.example.ex._1._5._17;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import org.example.ex._1._5._7.WeightedQuickUnionUF;
+
+public class ErdosRenyi {
+    public static void main(String[] args) {
+        StdOut.print("N: ");
+        final int N = StdIn.readInt();
+
+        final int connections = count(N);
+        StdOut.printf("connections: %d%n", connections);
+
+    }
+
+    private static int count(int N) {
+        int connections = 0;
+
+        final WeightedQuickUnionUF uf = new WeightedQuickUnionUF(N);
+
+        while (uf.count() > 1) {
+            final int p = StdRandom.uniform(N);
+            final int q = StdRandom.uniform(N);
+
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+            }
+            connections++;
+        }
+
+        return connections;
+    }
+}
